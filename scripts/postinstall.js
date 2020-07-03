858f3bd7a26d3f7d32891c3addb20036105bd397
Anthony Fu <anthonyfu117@hotmail.com>
2020-07-03 15:44:48 +0800

    fix: d.ts resloving, close #2

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -30,7 +30,7 @@ function installModules (names) {
 function switchVersion(version) {
   fs.writeFileSync(path.join(dir,'lib', 'index.cjs.js'), `module.exports = require('./v${version}/index.cjs')\n`, 'utf-8')
   fs.writeFileSync(path.join(dir,'lib', 'index.esm.js'), `export * from './v${version}/index.esm'\n`, 'utf-8')
-  fs.writeFileSync(path.join(dir,'lib', 'index.d.ts'), `export * from './v${version}/index.d.ts'\n`, 'utf-8')
+  fs.writeFileSync(path.join(dir,'lib', 'index.d.ts'), `export * from './v${version}/index'\n`, 'utf-8')
 }
 
 const Vue = loadModule('vue')
