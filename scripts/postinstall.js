e6798d16928853f70237c8cf71b24d5767ef1d9e
Anthony Fu <anthonyfu117@hotmail.com>
2020-08-04 21:05:31 +0800

    fix: failed on first install

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,17 +1,7 @@
-const { execSync } = require('child_process')
 const fs = require('fs')
 const path = require('path')
 
 const dir = path.resolve(__dirname, '..')
-const agent = (process.env.npm_config_user_agent || 'npm').split('/')[0]
-const userRoot = path.resolve(dir, '../..')
-
-const AgentCommands = {
-  npm: 'npm i',
-  yarn: 'yarn add',
-  pnpm: 'pnpm add',
-  cnpm: 'cnpm i',
-}
 
 function loadModule (name) {
   try {
@@ -19,12 +9,6 @@ function loadModule (name) {
   } catch (e) {
     return undefined
   }
-}
-
-function installModules (names) {
-  const command = AgentCommands[agent] || AgentCommands.npm
-  names = Array.isArray(names) ? names : [names]
-  execSync(`${command} ${names.join(' ')}`, { stdio: 'inherit', cwd: userRoot })
 }
 
 function switchVersion(version) {
@@ -39,10 +23,6 @@ if (!Vue || typeof Vue.version !== 'string') {
   console.warn('[vue-demi] Vue is not detected in the dependencies. Please install Vue first.')
 }
 else if (Vue.version.startsWith('2.')) {
-  const VCA = loadModule('@vue/composition-api')
-  if (!VCA) {
-    installModules(['@vue/composition-api'])
-  }
   switchVersion(2)
 }
 else if (Vue.version.startsWith('3.')) {
