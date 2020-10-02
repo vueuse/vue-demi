3484f132c81141bfdf6cf62e233b09363b4022c5
Sergej Sintschilin <seregpie@gmail.com>
2020-10-02 17:34:00 +0200

    export Vue version

--- lib/v2/index.d.ts
+++ lib/v2/index.d.ts
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 declare const isVue2: boolean
 declare const isVue3: boolean
+declare const version: string
 export * from '@vue/composition-api'
 export {
   Vue,
   isVue2,
   isVue3,
+  version,
 }
