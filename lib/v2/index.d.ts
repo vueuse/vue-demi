6adae2193832b29f6d53819ffdc7c006192726f6
Sergej Sintschilin <seregpie@gmail.com>
2020-10-02 16:42:38 +0200

    add isVue3

--- lib/v2/index.d.ts
+++ lib/v2/index.d.ts
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 declare const isVue2 = true
+declare const isVue3 = true
 export * from '@vue/composition-api'
 export {
   Vue,
   isVue2,
+  isVue3,
 }
--- lib/v3/index.d.ts
+++ lib/v3/index.d.ts
@@ -1,7 +1,9 @@
 import * as Vue from 'vue'
 declare const isVue2 = false
+declare const isVue3 = false
 export * from 'vue'
 export {
   Vue,
   isVue2,
+  isVue3,
 }
