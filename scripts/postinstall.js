339d8fd8872b332a4639d156c0f8823923aea871
Sergej Sintschilin <seregpie@gmail.com>
2020-10-02 16:38:40 +0200

    revert esm to es change

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -13,7 +13,7 @@ function loadModule(name) {
 
 function switchVersion(version) {
   fs.writeFileSync(path.join(dir, 'lib', 'index.cjs.js'), `module.exports = require('./v${version}/index.cjs')\n`, 'utf-8')
-  fs.writeFileSync(path.join(dir, 'lib', 'index.es.js'), `export * from './v${version}/index.es'\n`, 'utf-8')
+  fs.writeFileSync(path.join(dir, 'lib', 'index.esm.js'), `export * from './v${version}/index.esm'\n`, 'utf-8')
   fs.writeFileSync(path.join(dir, 'lib', 'index.d.ts'), `export * from './v${version}/index'\n`, 'utf-8')
 }
 
