62b7e0d4ba1e587e3ec72f701298674daa869d9e
Eduardo San Martin Morote <posva13@gmail.com>
2020-11-21 13:14:26 +0100

    feat: add warn function

--- lib/v2/index.d.ts
+++ lib/v2/index.d.ts
@@ -2,10 +2,12 @@ import Vue from 'vue'
 declare const isVue2: boolean
 declare const isVue3: boolean
 declare const version: string
+declare const warn: typeof console.warn
 export * from '@vue/composition-api'
 export {
   Vue,
   isVue2,
   isVue3,
   version,
+  warn
 }
