3229e5bcdcb9ff7fd2f552df68ee16541617f8b0
Sergej Sintschilin <seregpie@gmail.com>
2020-11-23 10:51:45 +0100

    export warn for Vue 2

--- lib/v2/index.d.ts
+++ lib/v2/index.d.ts
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 declare const isVue2: boolean
 declare const isVue3: boolean
+declare function warn(msg: string): void
 declare const version: string
 export * from '@vue/composition-api'
 export {
   Vue,
   isVue2,
   isVue3,
+  warn,
   version,
 }
