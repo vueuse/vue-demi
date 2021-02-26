eb5ecd24c30ebce841a245414c24f67bb5da5fc3
Anthony Fu <anthonyfu117@hotmail.com>
2021-02-26 23:15:41 +0800

    feat: polyfill `set` `del` for Vue 3, close #24

--- lib/v3/index.d.ts
+++ lib/v3/index.d.ts
@@ -3,6 +3,9 @@ declare const isVue2: boolean
 declare const isVue3: boolean
 declare const install: (vue?: any) => void
 
+export function set<T>(target: any, key: any, val: T): T
+export function del(target: any, key: any)
+
 export * from 'vue'
 export {
   Vue,
