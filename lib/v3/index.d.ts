5ebd7635cc25c4641099e883e59277272a0887ff
Anthony Fu <anthonyfu117@hotmail.com>
2021-03-01 17:48:06 +0800

    fix: typecheck for Vue 3

--- lib/v3/index.d.ts
+++ lib/v3/index.d.ts
@@ -7,10 +7,10 @@ declare const install: (vue?: any) => void
  * @deprecated To avoid bringing in all the tree-shakable modules, this API has been deprecated. Use `Vue2` or named exports instead.
  * Refer to https://github.com/vueuse/vue-demi/issues/41
  */
-declare const V: Vue
+declare const V: typeof Vue
 
 export function set<T>(target: any, key: any, val: T): T
-export function del(target: any, key: any)
+export function del(target: any, key: any): void
 
 export * from 'vue'
 export {
