f734be4721cfb0d84ae07fdcbfaa70594eb4cde7
ziga <zigomir@gmail.com>
2021-03-13 20:41:33 +0100

    fix: missing return type for del (#49)

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,7 +4,7 @@ declare const isVue3: boolean
 declare const install: (vue?: any) => void
 
 export function set<T>(target: any, key: any, val: T): T
-export function del(target: any, key: any)
+export function del(target: any, key: any): void
 
 export * from 'vue'
 export {
