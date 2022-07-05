3125ab6ba83486c011d6fc7faef15a3a5d3efc7e
Anthony Fu <anthonyfu117@hotmail.com>
2022-07-05 12:09:46 +0800

    feat(2.7): `createApp` polyfill

--- lib/v2.7/index.d.ts
+++ lib/v2.7/index.d.ts
@@ -24,3 +24,37 @@ export {
   version,
   install,
 }
+
+import Vue, { VueConstructor, VNode, VNodeDirective } from 'vue'
+
+// #region createApp polyfill
+export type DirectiveModifiers = Record<string, boolean>;
+export interface DirectiveBinding<V> extends Readonly<VNodeDirective> {
+    readonly modifiers: DirectiveModifiers;
+    readonly value: V;
+    readonly oldValue: V | null;
+}
+export type DirectiveHook<T = any, Prev = VNode | null, V = any> = (el: T, binding: DirectiveBinding<V>, vnode: VNode, prevVNode: Prev) => void;
+export interface ObjectDirective<T = any, V = any> {
+    bind?: DirectiveHook<T, any, V>;
+    inserted?: DirectiveHook<T, any, V>;
+    update?: DirectiveHook<T, any, V>;
+    componentUpdated?: DirectiveHook<T, any, V>;
+    unbind?: DirectiveHook<T, any, V>;
+}
+export type FunctionDirective<T = any, V = any> = DirectiveHook<T, any, V>;
+export type Directive<T = any, V = any> = ObjectDirective<T, V> | FunctionDirective<T, V>;
+export interface InjectionKey<T> extends Symbol {}
+export interface App<T = any> {
+  config: VueConstructor['config'];
+  use: VueConstructor['use'];
+  mixin: VueConstructor['mixin'];
+  component: VueConstructor['component'];
+  directive(name: string): Directive | undefined;
+  directive(name: string, directive: Directive): this;
+  provide<T>(key: InjectionKey<T> | symbol | string, value: T): this;
+  mount: Vue['$mount'];
+  unmount: Vue['$destroy'];
+}
+export declare function createApp(rootComponent: any, rootProps?: any): App;
+// #endregion
