5637e7bd0df13690a29d4c2148244fac516a3450
Anthony Fu <anthonyfu117@hotmail.com>
2022-07-05 12:40:56 +0800

    feat(2.7): createApp polyfill (#170)

--- .github/test.js
+++ .github/test.js
@@ -51,62 +51,93 @@ const mod = fs.readFileSync(resolve(DIR, `node_modules/vue-demi/lib/${indexFile}
 
 let failed = false
 
-if (isCjs && !mod.includes(`exports.isVue2 = ${isVue2}`)) {
-  console.log('CJS:', mod)
-  failed = true
-}
+;(function codeCheck() {
+  if (isCjs && !mod.includes(`exports.isVue2 = ${isVue2}`)) {
+    console.log('CJS:', mod)
+    failed = true
+  }
 
-if (!isCjs && !/export\s\{\n\s\sVue,\n\s\sVue2,\n\s\sisVue2/gm.test(mod)) {
-  console.log('ESM:', mod)
-  failed = true
-}
+  if (!isCjs && !/export\s*\{\s*Vue,\s*Vue2,\s*isVue2/gm.test(mod)) {
+    console.log('ESM:', mod)
+    failed = true
+  }
+})()
 
-const outputVersion = execSync(`node -e "console.log(require('vue-demi').version)"`, { cwd: DIR }).toString().trim()
-console.log('version: ' + outputVersion)
+;(function versionCheck() {
+  const outputVersion = execSync(`node -e "console.log(require('vue-demi').version)"`, { cwd: DIR }).toString().trim()
+  console.log('version: ' + outputVersion)
 
-// isVue2
-const is2 = execSync(`node -e "console.log(require('vue-demi').isVue2)"`, { cwd: DIR }).toString().trim()
+  // isVue2
+  const is2 = execSync(`node -e "console.log(require('vue-demi').isVue2)"`, { cwd: DIR }).toString().trim()
 
-if (is2 !== `${isVue2}`) {
-  console.log(`isVue2: ${is2} !== ${isVue2}`)
-  failed = true
-}
+  if (is2 !== `${isVue2}`) {
+    console.log(`isVue2: ${is2} !== ${isVue2}`)
+    failed = true
+  }
 
-const hasVue2 = execSync(`node -e "console.log(require('vue-demi').Vue2 !== undefined)"`, { cwd: DIR }).toString().trim()
+  const hasVue2 = execSync(`node -e "console.log(require('vue-demi').Vue2 !== undefined)"`, { cwd: DIR }).toString().trim()
 
-if (hasVue2 !== `${isVue2}`) {
-  console.log(`hasVue2: ${hasVue2} !== ${isVue2}`)
-  failed = true
-}
+  if (hasVue2 !== `${isVue2}`) {
+    console.log(`hasVue2: ${hasVue2} !== ${isVue2}`)
+    failed = true
+  }
+})()
 
-const importCJS = `const { ref, computed } = require('vue-demi');`
-const importESM = `const { ref, computed } = await import('vue-demi');`
+;(function reactivity() {
+  const importCJS = `const { ref, computed } = require('vue-demi');`
+  const importESM = `const { ref, computed } = await import('vue-demi');`
 
-const snippet = `
+  const snippet = `
 let a = ref(12)
 let b = computed(() => a.value * 2)
 console.log(b.value)
 a.value += 1
 console.log(b.value)
 `
-  .replace(/\n/g, ';')
-  .trim()
+    .replace(/\n/g, ';')
+    .trim()
 
-// ref
-const refCJS = execSync(`node -e "${importCJS}${snippet}"`, { cwd: DIR }).toString().trim()
-if (refCJS !== `24\n26`) {
-  console.log(`ref(cjs): ${refCJS} !== 24\n26`)
-  failed = true
-}
-
-// TODO: 2.7's ESM can't runs in Node currently
-if (!isVue27) {
-  const refESM = execSync(`node -e "(async ()=>{${importESM}${snippet}})()"`, { cwd: DIR }).toString().trim()
-  if (refESM !== `24\n26`) {
-    console.log(`ref(esm): ${refESM} !== 24\n26`)
+  // ref
+  const refCJS = execSync(`node -e "${importCJS}${snippet}"`, { cwd: DIR }).toString().trim()
+  if (refCJS !== `24\n26`) {
+    console.log(`ref(cjs): ${refCJS} !== 24\n26`)
     failed = true
   }
-}
+
+  // TODO: 2.7's ESM can't runs in Node currently
+  if (!isVue27) {
+    const refESM = execSync(`node -e "(async ()=>{${importESM}${snippet}})()"`, { cwd: DIR }).toString().trim()
+    if (refESM !== `24\n26`) {
+      console.log(`ref(esm): ${refESM} !== 24\n26`)
+      failed = true
+    }
+  }
+})()
+
+;(function createApp() {
+  const snippet = `
+const { createApp, h } = require('vue-demi');
+let app = createApp({
+  component: {
+    setup(props) {
+      return () => h('div', props.msg);
+    }
+  }
+}, {
+  msg: 'Hello'
+});
+
+console.log(!!app)
+`
+    .replace(/\n/g, '')
+    .trim()
+
+  const result = execSync(`node -e "${snippet}"`, { cwd: DIR }).toString().trim()
+  if (result !== `true`) {
+    console.log(`createApp(cjs): ${result} !== true`)
+    failed = true
+  }
+})()
 
 if (failed) {
   setTimeout(() => {
