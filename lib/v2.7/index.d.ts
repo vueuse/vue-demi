10e307e528d839598d8d1162ed40416ec24da12d
Eduardo San Martin Morote <posva@users.noreply.github.com>
2022-08-23 15:21:28 +0200

    feat: add warn to vue 2.7 (#189)

--- lib/v2.7/index.d.ts
+++ lib/v2.7/index.d.ts
@@ -1,11 +1,12 @@
 import Vue from 'vue'
-import type { PluginFunction, PluginObject, VueConstructor, Directive, InjectionKey } from 'vue'
+import type { PluginFunction, PluginObject, VueConstructor, Directive, InjectionKey, Component } from 'vue'
 
 declare const isVue2: boolean
 declare const isVue3: boolean
 declare const Vue2: typeof Vue | undefined
 declare const version: string
 declare const install: (vue?: typeof Vue) => void
+export declare function warn(msg: string, vm?: Component | null): void
 /**
  * @deprecated To avoid bringing in all the tree-shakable modules, this API has been deprecated. Use `Vue2` or named exports instead.
  * Refer to https://github.com/vueuse/vue-demi/issues/41
