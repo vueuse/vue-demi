ab06157c935dc9467a65373ce4737ececca477f9
zekunjin <zekun.jin@foxmail.com>
2024-02-29 14:41:41 +0800

    chore: export js file for require

--- scripts/utils.js
+++ scripts/utils.js
@@ -24,37 +24,12 @@ function copy(name, version, vue) {
   fs.writeFileSync(dest, content, 'utf-8')
 }
 
-function updateVue2API() {
-  const ignoreList = ['version', 'default']
-  const VCA = loadModule('@vue/composition-api')
-  if (!VCA) {
-    console.warn('[vue-demi] Composition API plugin is not found. Please run "npm install @vue/composition-api" to install.')
-    return
-  }
-
-  const exports = Object.keys(VCA).filter(i => !ignoreList.includes(i))
-
-  const esmPath = path.join(dir, 'index.mjs')
-  let content = fs.readFileSync(esmPath, 'utf-8')
-
-  content = content.replace(
-    /\/\*\*VCA-EXPORTS\*\*\/[\s\S]+\/\*\*VCA-EXPORTS\*\*\//m,
-`/**VCA-EXPORTS**/
-export { ${exports.join(', ')} } from '@vue/composition-api/dist/vue-composition-api.mjs'
-/**VCA-EXPORTS**/`
-    )
-
-  fs.writeFileSync(esmPath, content, 'utf-8')
-  
-}
-
 function switchVersion(version, vue) {
-  copy('index.cjs', version, vue)
+  copy('index.js', version, vue)
   copy('index.mjs', version, vue)
+  copy('index.global.js', version, vue)
   copy('index.d.ts', version, vue)
-
-  if (version === 2)
-    updateVue2API()
+  copy('index.d.mts', version, vue)
 }
 
 
